1908b5677d9512bc9d2e43a9bcaff9ff0bed5c10
GildardoMatias <Gildardo.358358@gmail.com>
2022-08-16 16:52:05 -0500

    Encuestas 2 build

--- src/recepcionViews/efectos.jsx
+++ src/recepcionViews/efectos.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { catalogo_efectos } from './efects_catalog'
-import { Form, Switch, Button } from 'antd'
+import { catalogo_efectos, desrealizacion, despersonalizacion, amnesia } from './efects_catalog'
+import { Form, Switch, Button, Radio, Space, Slider } from 'antd'
 
 export default function Efectos() {
   const onFinish = (values) => {
@@ -10,6 +10,7 @@ export default function Efectos() {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
+  const marks = { 0: '0', 10: '10' };
   return (
     <div className='mainContainer'>
       <h5>Efectos</h5>
@@ -37,6 +38,83 @@ export default function Efectos() {
             </Form.Item>
           })
         }
+
+        <h5>Desrealizacion</h5>
+        {
+          Object.keys(desrealizacion).map((pr) => {
+            return <Form.Item
+              label={pr}
+              name={pr}
+            >
+              <Radio.Group>
+                <Space direction="vertical">
+                  {
+                    desrealizacion[pr].map((rs) => <Radio value={rs}>{rs}</Radio>)
+                  }
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          })
+        }
+
+
+        <h5>Despersonalizacion</h5>
+        {
+          Object.keys(despersonalizacion).map((pr) => {
+            return <Form.Item
+              label={pr}
+              name={pr}
+            >
+              <Radio.Group>
+                <Space direction="vertical">
+                  {
+                    despersonalizacion[pr].map((rs) => <Radio value={rs}>{rs}</Radio>)
+                  }
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          })
+        }
+
+        <h5>Amnesia</h5>
+        {
+          Object.keys(amnesia).map((pr) => {
+            return <Form.Item
+              label={pr}
+              name={pr}
+              required
+            >
+              <Radio.Group>
+                <Space direction="vertical">
+                  {
+                    amnesia[pr].map((rs) => <Radio value={rs}>{rs}</Radio>)
+                  }
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          })
+        }
+
+        <Form.Item
+          label="a)	En general mi gusto por la ketamina IV es:"
+          name="ketamina"
+        >
+          <Slider min={0} max={10} marks={marks} dots />
+        </Form.Item>
+
+        <Form.Item
+          label="a.)	En general, cuánto anhela, apetece el tratamiento de la ketamina IV para otros efectos además de los efectos antidepresivos? "
+          name="ketaminaiv"
+        >
+          <Slider min={0} max={10} marks={marks} dots />
+        </Form.Item>
+
+        <Form.Item
+          label="b.)	Cuánto desea usar la ketamina IV en cantidades mayores con más frecuencia de lo que su psiquiatra le esta prescribiendo por razones distintas a esperar más efecto antidepresivo?  "
+          name="ketaminaivb"
+        >
+          <Slider min={0} max={10} marks={marks} dots />
+        </Form.Item>
 
         <Form.Item
           wrapperCol={{
